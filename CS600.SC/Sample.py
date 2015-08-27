dae84f93472db7efaf3c4fd0966c482bf1f599c3
Nyna <almera.nynaeve@gmail.com>
2015-08-27 06:45:00 +0200

    Added Problem Set7 (Simple Virus) Draft Problem Set8 (Resistant Virus)

--- CS600.SC/Sample.py
+++ CS600.SC/Sample.py
@@ -1,21 +1,46 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jun 22 07:33:48 2015
+Created on Mon Aug 17 07:40:55 2015
 
-@author: Jonathan Puvilland
+@author: Jonathan
 """
-x = 12345
-epsilon = 0.01
-answer = 0.0
-nbrGuesses = 0
+import pylab
+import random
 
-while abs(answer**2 - x) >= epsilon and answer <= x:
-    answer += 0.00001
-    nbrGuesses += 1
+def stdDev(X):
+    mean = sum(X)/float(len(X))
+    tot = 0.0
+    for x in X:
+        tot += (x - mean)**2
+    return (tot/len(X))**0.5
 
-print 'Nbr of guesses:', nbrGuesses
+def throwNeedles(numNeedles):
+    inCircle = 0
+    estimates = []
+    for Needles in xrange(1, numNeedles + 1, 1):
+        x = random.random()
+        y = random.random()
+        if (x*x + y*y)**0.5 <= 1.0:
+            inCircle += 1
+    return 4*(inCircle/float(Needles))
 
-if abs(answer**2 - x) >= epsilon:
-    print 'Failed on square root of', x
-else:
-    print answer, 'is close of square root of', x
+def estPi(precision = 0.01, numTrials = 20):
+    numNeedles = 1000
+    numTrials = 20
+    sDev = precision
+    while sDev >= (precision/2.0):
+        estimates = []
+        for t in range(numTrials):
+            piGuess = throwNeedles(numNeedles)
+            estimates.append(piGuess)
+        sDev = stdDev(estimates)
+        curEst = sum(estimates)/len(estimates) 
+        curEst = sum(estimates)/len(estimates)
+        print 'Est. = ' + str(curEst) +\
+              ', Std. dev. = ' + str(sDev)\
+              + ', Needles = ' + str(numNeedles)
+        numNeedles *= 2
+    return curEst
+
+estPi()
+    
